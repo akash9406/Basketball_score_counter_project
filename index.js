480ee2914f27a31a9e7edd33235cc7dc22d8fed0
akash9406 <90980359+akash9406@users.noreply.github.com>
2022-11-27 14:01:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+let con1 = document.getElementById("score1")
+let con2 = document.getElementById("score2")
+let val = 0
+let val2 = 0
+function plus1(){
+   val = val+1
+   con1.textContent = val
+}
+function plus2(){
+    val = val+2
+    con1.textContent = val
+}
+function plus3(){
+    val = val+3
+    con1.textContent = val
+}
+function plushome1(){
+    val2 += 1
+    con2.textContent = val2
+}
+function plushome2(){
+    val2 += 2
+    con2.textContent = val2
+}
+function plushome3(){
+    val2 += 3
+    con2.textContent = val2
+}
